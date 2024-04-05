e1d0f0245b1931e9fb79d9e94e01e2d2db6540b2
Itoshiko Nurahman <itoshikonurahman@gmail.com>
2024-04-05 09:12:04 +0700

    build: add model and connection to mongoose

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const path = require('path')
 const app = express()
+
+// import model
+const Place = require('./model/place')
+
+// connet mongoose databases
+mongoose.connect('mongodb://localhost:27017/bestpoints')
+.then(()=>{console.log('Connncted to Databases')})
+.catch((err)=>{console.log(err)})
 
 // set view engine and views directory
 app.set('view engine', 'ejs')
@@ -9,6 +18,18 @@ app.set('views', path.join(__dirname, 'views'))
 // render halaman
 app.get('/', (req, res)=>{
     res.render('home')
+})
+
+app.get('/seed/place', async (req, res)=>{
+    const place = new Place({
+        title: 'Pantai Kuta',
+        price: 'Rp. 100.000',
+        description: 'Pantai yang indah',
+        location: 'Bali'
+    })
+
+    await place.save()
+    res.send(place)
 })
 
 // setting port
